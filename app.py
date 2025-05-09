ab9a1b1bc8aa7c3f8d272368c120dc3f3e5e6296
Masa658 <masaoka@yumoto.jp>
2025-05-09 15:07:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 import os
 
 app = Flask(__name__)
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+client = openai(api_key=os.environ.get("OPENAI_API_KEY"))
 
 faq_context = """
 YUMO PARTSのよくある質問：
